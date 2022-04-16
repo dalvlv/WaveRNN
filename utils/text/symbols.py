3b8a80ebda51c81e60d41b044da68d923acf74c3
lihaoqi <erfanshu@weierai.com>
2022-04-16 10:10:15 +0800

    修改英文编码为中文

--- utils/text/symbols.py
+++ utils/text/symbols.py
@@ -7,12 +7,14 @@ The default is a set of ASCII characters that works well for English or text tha
 from utils.text import cmudict
 
 _pad = '_'
-_punctuation = '!\'(),.:;? '
-_special = '-'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-
+# _punctuation = '!\'(),.:;? '
+# _special = '-'
+# _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
-_arpabet = ['@' + s for s in cmudict.valid_symbols]
-
+# _arpabet = ['@' + s for s in cmudict.valid_symbols]
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+# symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+
+_chinese_punctuation = '—…，。、：！？；（）“” '
+_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+symbols = [_pad] + list(_chinese_punctuation) + list(_characters)
